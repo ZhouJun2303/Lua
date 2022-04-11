6fb41a006026a41ffc7a6ecf9b12ad414db6c888
JUN-ZHOU <478103223@qq.com>
2022-04-11 10:57:40 +0800

    循环

---
+++ Demo4.lua
@@ -0,0 +1,17 @@
+-- 循环
+num1 = 0;
+while num1<5 do
+    num1 = num1+1
+    print(num1)
+    
+end
+print("--------------------------")
+for i = 1, 10, 1 do
+    print(i)
+end
+print("--------------------------")
+a=0
+repeat
+    a= a+1
+    print(a)
+until (a%5==0)
